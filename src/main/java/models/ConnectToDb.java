d370406e9b52355fd1bb148e127f9bda370bb7be
Fatima DAKAK <104937526+fatimaDakak@users.noreply.github.com>
2023-11-03 20:29:35 +0100

    Update ConnectToDb.java

--- src/main/java/models/ConnectToDb.java
+++ src/main/java/models/ConnectToDb.java
@@ -1,11 +1,9 @@
-package models;
-
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class ConnectToDb {
-    private static final String DB_URL = "jdbc:mysql://mysql:3306/employee-management";
+    private static final String DB_URL = "jdbc:mysql://mysql:3306/employee_management";
     private static final String DB_USER = "root";
     private static final String DB_PASSWORD = "";
 
@@ -18,14 +16,15 @@ public class ConnectToDb {
         }
     }
 
-    public static Connection getConnection() throws SQLException {
-        Connection connection = DriverManager.getConnection(DB_URL, DB_USER, DB_PASSWORD);
-        if (connection != null) {
+    public static Connection getConnection() {
+        Connection connection = null;
+        try {
+            connection = DriverManager.getConnection(DB_URL, DB_USER, DB_PASSWORD);
             System.out.println("Connection to the database successful.");
-        } else {
+        } catch (SQLException e) {
             System.err.println("Failed to connect to the database.");
+            e.printStackTrace();
         }
         return connection;
     }
-   
 }
